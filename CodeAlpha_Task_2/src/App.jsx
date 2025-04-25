ed3a9ab96011cb885d0bf11e1be586ad079ff28c
Nitika kumari <nitikas1299@gmail.com>
2025-04-25 23:23:41 +0530

    Add files via upload

---
+++ CodeAlpha_Task_2/src/App.jsx
@@ -0,0 +1,13 @@
+import MusicPlayer from './components/MusicPlayer'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Music Player</h1>
+      <MusicPlayer />
+    </div>
+  )
+}
+
+export default App
